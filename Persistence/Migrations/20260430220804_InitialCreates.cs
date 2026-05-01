802d4aeeda96cd8aa0aadb5b8820fb2fc2bfbff6
Mehrad44 <mehrad.langrud@outlook.com>
2026-05-02 01:29:56 +0330

    Git init

---
+++ Persistence/Migrations/20260430220804_InitialCreates.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Latitiude",
+                table: "Activities",
+                newName: "Latitude");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Latitude",
+                table: "Activities",
+                newName: "Latitiude");
+        }
+    }
+}
