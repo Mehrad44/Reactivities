b38619ba877b32447c2d399e3b4911427cb2df15
Mehrad <mehrad.langrud@outlook.com>
2026-07-13 01:08:19 +0330

    Add About me

--- client/src/features/profiles/ProfileAbout.tsx
+++ client/src/features/profiles/ProfileAbout.tsx
@@ -1,30 +1,30 @@
 import { useParams } from "react-router"
-import { useProfile } from "../../lib/types/hooks/useProfile";
 import { Box, Button, Divider, Typography } from "@mui/material";
-
+import { useState } from "react";
+import ProfileEdit from "./ProfileEditForm";
+import { useProfile } from "../../lib/types/hooks/useProfile";
 export default function ProfileAbout() {
-
-    const {id} = useParams();
-    const {profile} = useProfile(id);
-
-    return(
-
+    const { id } = useParams();
+    const { profile, isCurrentUser } = useProfile(id);
+    const [editMode, setEditMode] = useState(false);
+    return (
         <Box>
-            <Box sx={{display: 'flex' , justifyContent:'space-between'}}>
+            <Box sx={{display:'flex', justifyContent:'space-between'}}>
                 <Typography variant="h5">About {profile?.displayName}</Typography>
-                <Button>
-                    Edit Profile
-                </Button>
+                {isCurrentUser &&
+                    <Button onClick={() => setEditMode(!editMode)}>
+                        Edit profile
+                    </Button>}
             </Box>
-            <Divider sx={{my:2}}/>
-            <Box sx={{overflow: 'auto',maxHeight:350}}>
-                <Typography variant="body1" sx={{whiteSpace: 'pre-wrap'}}>
-                    {profile?.bio || 'No description added'} 
+            <Divider sx={{ my: 2 }} />
+            <Box sx={{ overflow: 'auto', maxHeight: 350 }}>
+                <Typography variant="body1" sx={{ whiteSpace: 'pre-wrap' }}>
+                    {profile?.bio || 'No description added yet'}
                 </Typography>
-
+                {editMode && (
+                    <ProfileEdit setEditMode={setEditMode} />
+                )}
             </Box>
         </Box>
-     
     )
-  
 }
